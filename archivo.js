86da52529660095281c5cba403d866ac0b214ccc
Agus-Sosa <sosaagustin351@gmail.com>
2022-07-22 02:58:24 -0300

    agregue y modifique un poco sass

--- archivo.js
+++ archivo.js
@@ -9,3 +9,10 @@ window.onload = function () {
         
     }
 }
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+myInput.focus()
+})
